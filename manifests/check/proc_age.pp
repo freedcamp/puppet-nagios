e398a9dde5b83b04c8f89da919fb3ba344ab5909
Igor Kryltsov <ivk@deepshiftlabs.com>
2018-02-13 16:33:03 +1100

    Further fixes on proc_age

--- manifests/check/proc_age.pp
+++ manifests/check/proc_age.pp
@@ -25,9 +25,13 @@ class nagios::check::proc_age (
   #$fullargs = strip("${arg_h}${arg_p}${arg_u}-f ${args}")
 
 
-  # https://exchange.nagios.org/directory/Plugins/Email-and-Groupware/Postfix/Check-Postfix-Mailqueue-2/details  --> check_phpfpm_status.pl -H 127.0.0.1 -s nagios.example.com -w 1,1,1 -c 0,2,2
+  # https://exchange.nagios.org/directory/Plugins/Operating-Systems/Linux/check_proc_age-2Esh/details
+  # Usage : check_proc_age.sh -p -w -c
+  # -p parameter : name of the monitoring process
+  # -c parameter : minimal elapsed time for status CRITICAL on NAGIOS
+  # -w parameter : minimal elapsed time for status WARNING on NAGIOS
   # Include defaults if no overrides in $args
-  if !$args { $fullargs = '-w 20 -c 50'}
+  if !$args { $fullargs =  '-p "fc_cron" -c 10800 -w 3600'}
 
   nagios::client::nrpe_file { 'check_proc_age':
     ensure => $ensure,
@@ -36,7 +40,7 @@ class nagios::check::proc_age (
 
   nagios::service { "check_proc_age_${check_title}":
     ensure                   => $ensure,
-    check_command            => "check_proc_age!${fullargs}",
+    check_command            => "check_nrpe_proc_age!${fullargs}",
     service_description      => 'process age',
     servicegroups            => $servicegroups,
     check_period             => $check_period,
