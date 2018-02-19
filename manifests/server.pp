3407cf61c9aa918f696b7c198b528b3e686dfd49
Igor Kryltsov <ivk@deepshiftlabs.com>
2018-02-19 21:17:09 +1100

    Added dependency package

--- manifests/server.pp
+++ manifests/server.pp
@@ -175,18 +175,28 @@ class nagios::server (
     package { 'perl-IO-Socket-SSL':
         ensure => 'absent',
     }
-        package { 'perl-libwww-perl':
+    package { 'perl-libwww-perl':
         ensure => 'absent',
     }
   }
-    if $plugin_apache_status {
-      file { "${plugin_dir}/check_apache_status":
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0755',
-        content => template('nagios/plugins/check_apache_status'),
-      }
+  if $plugin_apache_status {
+    file { "${plugin_dir}/check_apache_status":
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0755',
+      content => template('nagios/plugins/check_apache_status'),
     }
+        ensure_packages([
+        'nagios-plugins-perl',
+    ])
+  } else {
+    file { "${plugin_dir}/check_apache_status":
+      ensure => absent,
+    }
+    package { 'nagios-plugins-per':
+        ensure => 'absent',
+    }
+  }
   # Other packages
   # For the default email notifications to work
   ensure_packages(['mailx'])
